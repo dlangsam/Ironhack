23cd74d3ab566fa24da5306cdfd81d2e427675a7
Devorah Langsam <devorah@langsam.org>
2016-06-27 09:47:19 -0400

    Change made to player spec

--- Week2/movie_game/spec/player_spec.rb
+++ Week2/movie_game/spec/player_spec.rb
@@ -3,13 +3,10 @@ require_relative("../lib/player.rb")
 
 
 RSpec.describe 'Player tests' do
-
-
   let :player do 
     Player.new
   end
  it "player starts up with zero questions or score" do
- 
     expect(player.score).to eq(0)
     expect(player.questions_answered). to eq(0)
   end
@@ -18,7 +15,6 @@ RSpec.describe 'Player tests' do
     expect(player.score).to eq(0)
     expect(player.questions_answered). to eq(1)
   end
-
   it "tests answered_right method to make sure score and questions_answered increases" do
  	player.answered_right
     expect(player.score).to eq(1)
