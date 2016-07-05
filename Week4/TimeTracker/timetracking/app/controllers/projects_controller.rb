0af17b0a611c8911bcf34b18a3fb56baec430d68
Devorah Langsam <devorah@langsam.org>
2016-07-05 14:15:38 -0400

    Add project controller

---
+++ Week4/TimeTracker/timetracking/app/controllers/projects_controller.rb
@@ -0,0 +1,6 @@
+class ProjectsController < ApplicationController
+	def index
+		@projects_array = Project.order(created_at: "desc").limit(10)
+		render 'index'
+	end
+end
--- Week4/TimeTracker/timetracking/app/models/project.rb
+++ Week4/TimeTracker/timetracking/app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ApplicationRecord
+	def index
+		render 'index'
+	end
 end
