178c3b62c29b0f54e526d2cbddcd7d634ec61bc3
Devorah Langsam <devorah@langsam.org>
2016-07-07 14:19:00 -0400

    Refactor to be more dry, add strong parameters and partials for time entry form

--- Week4/TimeTracker/timetracking/app/controllers/time_entries_controller.rb
+++ Week4/TimeTracker/timetracking/app/controllers/time_entries_controller.rb
@@ -13,18 +13,34 @@ class TimeEntriesController < ApplicationController
 
 	def create
 		@my_project = Project.find(params[:project_id])
-		@my_entry = @my_project.time_entries.new(
-			:hours => params[:time_entry][:hours],
-			:minutes => params[:time_entry][:minutes],
-			:date => params[:time_entry][:date],
-			:comments => params[:time_entry][:comments]
-
-			)
+		@my_entry = @my_project.time_entries.new(entry_params)
 
 		if @my_entry.save
 			redirect_to "/projects/#{@my_project.id}/time_entries"
 		else
 			render 'new'
 		end
+	end
+
+	def edit
+		@my_project = Project.find(params[:project_id])
+		@my_entry = @my_project.time_entries.find(params[:id])
+		render 'edit'
+	end
+
+	def update
+		@my_project = Project.find(params[:project_id])
+		@my_entry = @my_project.time_entries.find(params[:id])
+		if  @my_entry.update(entry_params)
+			redirect_to action: "index", controller: "time_entries", project_id: @my_project.id
+		else
+			render "edit"
+		end
+	end
+
+	private
+	#this is a utility method, not accessed by routes
+	def entry_params
+		params.require(:time_entry).permit(:hours, :minutes, :date, :comments)
 	end
 end
