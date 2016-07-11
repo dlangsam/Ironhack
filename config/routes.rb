7527f5c8e338c06d99f301c449a3487572dc0768
Devorah Langsam <devorah@langsam.org>
2016-07-11 11:58:55 -0400

    Remove workcentral from git

--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  get '/' => 'visitors#home'
-  get '/about' => 'visitors#about', as: :about
-  get '/blog' => 'visitors#blog'
-  get '/contact' => 'visitors#contact'
-end
